d5127e13dcf81196c1367fbc0e94d1eb64e8e3a4
simon <simom@52g.team>
2023-09-28 14:54:45 +0900

    branch study7 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+      primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+
+    );
+  }
+}
