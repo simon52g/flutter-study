1352f7b2494fecd0e9c02d1800df9e3c16fb6bcd
simon <simom@52g.team>
2023-10-02 07:59:27 +0900

    study16

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,16 @@ class Mypage extends StatelessWidget {
                 backgroundImage: AssetImage('assets/3.gif'),
                 backgroundColor: Colors.white,
             ),
+            otherAccountsPictures: <Widget> [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/1.png'),
+                backgroundColor: Colors.white,
+              ),
+              // CircleAvatar(
+              //   backgroundImage: AssetImage('assets/2.png'),
+              //   backgroundColor: Colors.white,
+              // ),
+            ],
             accountName: Text('BBANTO'),
             accountEmail: Text('bbanto@bbanto.com'),
             onDetailsPressed: () {
@@ -65,10 +75,42 @@ class Mypage extends StatelessWidget {
                 bottomRight: Radius.circular(40.0)
               )
             ),
-            )
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: (){
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+                        ListTile(
+              leading: Icon(Icons.settings,
+              color: Colors.grey[850],
+              ),
+              title: Text('Setting'),
+              onTap: (){
+                print('Setting is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+                        ListTile(
+              leading: Icon(Icons.question_answer,
+              color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: (){
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+//순한맛 강자 16까지 함.
