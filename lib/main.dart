6eb2417afa365f1a625431768d252f4cb4d43310
simon <simom@52g.team>
2023-10-05 20:58:57 +0900

    main.dart 수정
    Scaffold.of -> ScaffoldMessenger.of로 변경함

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Snack Bar',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home : Mypage()
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Mypage(),
     );
   }
 }
@@ -26,8 +24,24 @@ class Mypage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Snack Bar'),
         centerTitle: true,
-
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red, // Background color
+          ),
+          child: Text(
+            'Show me',
+            style: TextStyle(color: Colors.white),
+          ),
+          // color: Colors.red,
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Hellow'),
+            ));
+          },
         ),
-      );
+      ),
+    );
   }
 }
