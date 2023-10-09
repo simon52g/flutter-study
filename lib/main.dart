4f71258bf2890c787f0af6b7b89c90f78c6c0acd
simon <simom@52g.team>
2023-10-10 03:16:47 +0900

    main.dart
    마지막 Elevated button, Text button 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,54 +7,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Myapp',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
-
-  @override
-  Widget build(BuildContext context2) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fist page'),
+        title: Text('Button'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the Second page'),
-          onPressed: () { 
-            Navigator.push(
-              context2, MaterialPageRoute(
-                builder: (context) =>SecondPage()
-                ));
-          },
-        ),
-      ),
-    );
-  }
-}
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                // onPressed: (){
+                //   print('text button');
+                // },
+                onPressed: () {
+                  print('Text button');
+                },
+                child: Text(
+                  'Text button',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                style: TextButton.styleFrom(
+                  primary: Colors.red,
+                  // backgroundColor: Colors.blue
+                )),
+            ElevatedButton(
+              onPressed: () {
+                print('Elevated button');
+              },
+              child: Text('Elevated button'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  elevation: 0.0),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print('Outline button');
+              },
+              child: Text('Outline button'),
+              style: OutlinedButton.styleFrom(primary: Colors.green
+                  // side: BorderSide(
 
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the First page'),
-          onPressed: () {
-            Navigator.pop(ctx);
-           },
+                  // )
+                  ),
+            ),
+            TextButton.icon(
+              onPressed: () {
+                print('icon button');
+              },
+              icon: Icon(
+                Icons.home,
+                size: 30.0,
+                color: Colors.black87,
+              ),
+              label: Text('Go to home'),
+              style: TextButton.styleFrom(primary: Colors.purple),
+            )
+          ],
         ),
       ),
     );
