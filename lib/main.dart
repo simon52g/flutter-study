43ae304c502f9b51c10df216574c1b0f3dc0a1d3
simon <simom@52g.team>
2023-10-10 02:26:05 +0900

    main.dart에 수정함
    기존 study22에서 import 한 파일을 지우고 나서 핫리로드를 해도 이상한
    페이지가다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/ScreenB.dart';
-import 'package:flutter_application_1/ScreenC.dart';
-import 'ScreenA.dart';
+// import 'package:flutter_application_1/ScreenB.dart';
+// import 'package:flutter_application_1/ScreenC.dart';
+// import 'ScreenA.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,13 +10,112 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/' :(context) => ScreenA(),
-        '/b' :(context) => ScreenB(),
-        '/c' :(context) => ScreenC()
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scafflod Messengr'),
+      ),
+      body: HomeBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Like a new Snakbar'),
+              duration: Duration(seconds: 5),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ThirdPage()),
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Text('Go to the Second page'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SeconPage()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SeconPage extends StatelessWidget {
+  const SeconPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Text(
+          '"좋아요가 추가 되었습니다.',
+          style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+        ),
+        body: Builder(
+          builder: (context){
+            return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '"좋아요를 취소하시겠습니까?"',
+                style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text('"좋아요가 취소되었습니다"'),
+                        duration: Duration(seconds: 3)),
+                  );
+                },
+                child: Text('취소하기'),
+              ),
+            ],
+          ),
+        );
+        }
+      ),
+      ),
+    );
+  }
+}
+
+// Flutter 2.0으로변하면서 Snack bar -> ScaffoldMessenger로 왜 변경이 되는었는지 설명 
+// 여기저기 흩어져 있는 자손 Scaffold를 관리하기 위해서 변경 됨
