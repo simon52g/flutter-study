ea9927aea2eaf63e4a6ddd1c5eca06c252fc1ac0
simon <simom@52g.team>
2023-10-24 18:20:56 +0900

    study26 새로운 파일 저장

---
+++ flutter_application_2/lib/reponsive/responsive_center.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveCenter extends StatelessWidget {
+  const ResponsiveCenter({super.key});
+  final double maxContentWidth;
+  final EdgeInsetsGeometry padding;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+//ctrl + . 눌러서 생성자를 만들어주겠음 부터 못 따라 가겠음. 
